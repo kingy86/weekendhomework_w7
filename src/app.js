0e897811ed797c311d92bc32ba5474b00dae0716
Mark <nirvanafoo86@hotmail.com>
2019-04-14 13:32:54 +0100

    Preparing files for project

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const Beers = require('./models/beers.js');
+const BeersView = require('./views/beers_view.js')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+});
